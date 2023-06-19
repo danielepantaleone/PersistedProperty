cf5bf9ad761393fc1b78c9b6e3cad8438a9555c7
Daniele Pantaleone <danielepantaleone@me.com>
2023-06-19 16:16:24 +0200

    Specify minimum supported version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "PersistedProperty",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
         .library(
             name: "PersistedProperty",
